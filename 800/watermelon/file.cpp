dbc34b132d90d41c481422537df7e495a206aa9c
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-04 20:47:18 +0500

    Update file.cpp

--- 800/watermelon/file.cpp
+++ 800/watermelon/file.cpp
@@ -1,1 +1,22 @@
+// https://codeforces.com/problemset/problem/4/A
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w;
+    cout << "Enter the weight of the watermelon: ";
+    cin >> w;
+    if(w < 1 || w > 100) {              // check if the weight is lss than 1 or greater than 100
+        cout << "Invalid weight.\n";
+    }
+    else if(w >= 1 && w <= 100) {       // check for the weight between 1 and 100 inclusive
+        if(w % 2 == 0) {                // check if the weight is even number or not
+            cout << "YES.";
+        }
+        else {
+            cout << "NO";
+        }
+    }
+    return 0;
+}
