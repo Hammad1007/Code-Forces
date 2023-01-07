fe590ad288b57c92cdfa83759e770cef86b95e63
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-07 22:40:19 +0500

    Create file.py

---
+++ 800/YES or YES?/file.py
@@ -0,0 +1,13 @@
+def compare(s):
+  if len(s) < 3 or len(s) > 3:
+    print("Invalid.\n")
+  elif len(s) == 3:
+    if ((s[0] == 'y' or s[0] == 'Y') and (s[1] == 'e' or s[1] == 'E') and (s[-1] == 's' or s[-1] == 'S')):
+      print("Yes.\n")
+    else:
+      print("No.\n")
+
+n = int(input())
+for i in range(n):
+  temp = input()
+  compare(temp)
