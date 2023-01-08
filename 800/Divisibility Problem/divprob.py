c177d3b95188384f1e506585f80ea5969d9532ed
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-08 13:08:29 +0500

    Create divprob.py

---
+++ 800/Divisibility Problem/divprob.py
@@ -0,0 +1,17 @@
+
+
+def divisibleby(x, y):
+  if x % y == 0:
+    temp = 0
+  else:
+    temp = x % y
+    temp = y - temp
+  return temp
+
+n = int(input())
+for i in range(n):
+  a = int(input())
+  b = int(input())
+  print("Values of a and b are:", a, "and", b)
+  val = divisibleby(a, b)
+  print("The value is: ", val)
