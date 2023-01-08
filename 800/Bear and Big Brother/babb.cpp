68c5cbe07c9b08fdaf344a5b844ff4033d720519
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-08 17:58:11 +0500

    Update babb.cpp

--- 800/Bear and Big Brother/babb.cpp
+++ 800/Bear and Big Brother/babb.cpp
@@ -1,1 +1,36 @@
+// https://codeforces.com/problemset/problem/791/A
 
+#include <iostream>
+using namespace std;
+
+// Main driver
+int main() {
+    int a, b, count = 1;      // a is Limak's weight and b is Bob's weight
+    cout << "Enter Limak's weight: ";
+    cin >> a;
+    cout << "Enter Bob's weight: ";
+    cin >> b;
+    if((a >= 1 && a <= 10) && (b >= 1 && b <= 10)) {
+        if(a <= b) {
+            if(a == b) {
+                count = 1;
+            }
+            else {
+                while(a <= b) {
+                    a = a * 3;
+                    b = b * 2;
+                    if(a <= b) {
+                        count++;
+                    }
+                }
+                
+            }
+        }
+        cout << "The count is: " << count << endl;
+    }
+    else {
+        cout << "Invalid input.\n";
+    }
+    
+    return 0;
+}
