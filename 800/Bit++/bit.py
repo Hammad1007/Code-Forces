20e30f79ea0cbd0ce601746e1c8d247e413d43f9
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-09 21:20:01 +0500

    Create bit.py

---
+++ 800/Bit++/bit.py
@@ -0,0 +1,15 @@
+# https://codeforces.com/problemset/problem/282/A
+
+a = []
+x = 0
+n = int(input("Enter the number: "))
+for i in range(0, n):
+  l = input()
+  a.append(l)
+print("The array is: ", a)
+for i in range(0, n):
+  if a[i] == '+':
+    x = x + 1
+  elif a[i] == '-':
+    x = x - 1
+print("The value of x is: ", x)
