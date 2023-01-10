380b6181679e55a790cc60f047bf02e1fc159459
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-10 10:42:43 +0500

    Create wordcapitalize.py

---
+++ 800/Word Capitalization/wordcapitalize.py
@@ -0,0 +1,7 @@
+# https://codeforces.com/problemset/problem/281/A
+
+word = input("Enter the string: ")
+print("Th original word is: ", word)    # original string
+
+res = word[0].upper() + word[1:]        # first letter is upper while rest are as it is
+print("The converted sting is: ", res)
