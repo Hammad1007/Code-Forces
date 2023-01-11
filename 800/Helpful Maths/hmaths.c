17bdb57838e82bd583ceb41fd65681e1d94730b6
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-11 15:23:26 +0500

    Update hmaths.c

--- 800/Helpful Maths/hmaths.c
+++ 800/Helpful Maths/hmaths.c
@@ -1,1 +1,36 @@
 // https://codeforces.com/problemset/problem/339/A
+
+// https://codeforces.com/problemset/problem/266/A
+
+#include <stdio.h>
+
+// Main driver
+int main() {
+    char arr[50]; 
+    int n, i, j;
+    printf("Enter the size: ");
+    scanf("%d", &n);
+    printf("Enter the string: ");
+    for(i = 0; i < n; i++) {
+        printf("Enter the data: ");
+        scanf("%s", &arr[i]);
+    }
+    printf("The array is: ");           
+    for(i = 0; i < n; i++) {
+        printf("%s", arr);       
+    }
+    for(i = 0; i < n; i = i + 2) {
+        for(j = i + 2; j < n; j = j + 2) {
+            if(arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(i = 0; i < n; i++) {
+        printf("%s", arr);       
+    }
+
+    return 0;
+}
