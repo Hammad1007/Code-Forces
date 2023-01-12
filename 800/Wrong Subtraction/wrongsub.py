2004795a2498c5d1223aef960b0e9f91aa52d8fc
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-12 12:46:04 +0500

    Update wrongsub.py

--- 800/Wrong Subtraction/wrongsub.py
+++ 800/Wrong Subtraction/wrongsub.py
@@ -1,1 +1,13 @@
 # https://codeforces.com/problemset/problem/977/A
+
+n = int(input("Enter the number: "))
+k = int(input("Enter the number of operations: "))
+print("Number = ", n)
+print("Operatons to be performed = ", k)
+for i in range(k):
+  if n % 10 != 0:
+    n = n - 1
+  elif n % 10 == 0:
+    n = n / 10
+
+print("The new value of number: ", n)
