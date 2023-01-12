689e3d4e5d6d06ec4821d177d437df2596cd1b96
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-12 12:42:27 +0500

    Update wrongsub.c

--- 800/Wrong Subtraction/wrongsub.c
+++ 800/Wrong Subtraction/wrongsub.c
@@ -1,1 +1,23 @@
 // https://codeforces.com/problemset/problem/977/A
+
+#include <stdio.h>
+
+// Main driver
+int main() {
+    int n, k;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    printf("Enter the number of operations you want to perform: ");
+    scanf("%d", &k);
+    while(k > 0) {
+        if(n % 10 != 0) {
+            n = n - 1;
+        }
+        else if(n % 10 == 0) {
+            n = n / 10;
+        }
+        k--;
+    }
+    printf("The number is: %d", n);
+    return 0;
+}
