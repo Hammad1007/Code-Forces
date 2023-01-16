f52f4131f4661b8951fce598b8590c3bc944a23f
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-16 13:37:46 +0500

    Update lucky.py

--- 800/Lucky?/lucky.py
+++ 800/Lucky?/lucky.py
@@ -1,1 +1,18 @@
 # https://codeforces.com/problemset/problem/1676/A
+
+n = int(input("Enter the test cases: "))
+print("The test cases are: ", n)
+
+while n > 0:
+    a = []      # empty array
+    for i in range(6):
+        l = int(input())
+        a.append(l)
+    print("The array is: ", a)
+    
+    if a[0] + a[1] + a[2] == a[3] + a[4] + a[5]:
+        print("Yes.\n")
+    else:
+        print("No.\n")
+        
+    n = n - 1       # decrement in test cases
