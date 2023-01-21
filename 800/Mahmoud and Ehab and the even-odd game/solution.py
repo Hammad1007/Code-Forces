3c4484c72cf011db226092f5f23f05065d2ac5ef
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-21 11:14:34 +0500

    Create solution.py

---
+++ 800/Mahmoud and Ehab and the even-odd game/solution.py
@@ -0,0 +1,8 @@
+# https://codeforces.com/problemset/problem/959/A
+
+n = int(input("Enter the number: "))
+print(f"The number is {n}.")
+if n % 2 == 0:
+  print("Mahmoud.\n")
+else:
+  print("Ehab.\n")
