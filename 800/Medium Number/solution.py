c6dbbcfdba7dbd4e1ec6f37b01f6864211fceb07
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-09 20:43:34 +0500

    Update solution.py

--- 800/Medium Number/solution.py
+++ 800/Medium Number/solution.py
@@ -8,4 +8,10 @@ while t > 0:
   b = int(input("Enter b: "))
   c = int(input("Enter c: "))
   print(f"The values are: {a}, {b} and {c}")
-  
+  if (a > b and a < c) or (a < b and a > c):
+    print("The medium number is: ", a)
+  elif (b > a and b < c) or (b < a and b > c):
+    print("The medium number is: ", b)
+  elif (c > a and c < b) or (c < a and c > b):
+    print("The medium numebr is: ", c)
+  t = t - 1
