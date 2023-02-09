80c7b4ecee59c106df5b682d3608c7b262d45122
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-09 20:23:02 +0500

    Update solution.py

--- 800/Medium Number/solution.py
+++ 800/Medium Number/solution.py
@@ -1,1 +1,11 @@
-# Solve here
+# https://codeforces.com/problemset/problem/1760/A
+
+t = int(input("Enter the number of test cases: "))
+print("The number of test cases are: ", t)
+
+while t > 0:
+  a = int(input("Enter a: "))
+  b = int(input("Enter b: "))
+  c = int(input("Enter c: "))
+  print(f"The values are: {a}, {b} and {c}")
+  
