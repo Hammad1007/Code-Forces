4ab0be306aede0b9f5fcc24f5b2b97bfed4c4e5c
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-23 12:01:52 +0500

    Update solution.py

--- 800/C+=/solution.py
+++ 800/C+=/solution.py
@@ -2,3 +2,15 @@
 
 t = int(input("Enter the number of test cases: "))
 print("The test cases are: ", t)
+count = 0
+while t > 0:
+  a = int(input("Enter a: "))
+  b = int(input("Enter b: "))
+  n = int(input("Enter n: "))
+  print("The values are: ", a, " ", b, " ", n)
+  a = a + b
+  while a <= n:
+    count = count + 1
+    a = a + b
+  print("Count is: ", count + 1)
+  t = t - 1
